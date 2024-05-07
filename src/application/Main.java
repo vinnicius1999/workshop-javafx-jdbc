3efe2edd9331a46a5a389dfe6d5c15e820cf5b53
Vinicius Barbosa <viniciusbarbosa934@gmail.com>
2024-05-07 17:31:12 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,22 +4,24 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane ScrollPane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			ScrollPane.setFitToHeight(true);//faz a barra ir até o final da caixa do programa
-			ScrollPane.setFitToWidth(true);//faz a barra ir até o final da caixa do programa
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(ScrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -28,6 +30,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
